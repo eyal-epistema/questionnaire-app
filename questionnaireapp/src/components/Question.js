c2d804bc985a71b57ad70fdecdec1b015b144a85
Eyal Zach <eyal.zach@episte.ma>
2016-06-23 12:38:51 +0300

    added question component

--- questionnaireapp/src/components/Question.js
+++ questionnaireapp/src/components/Question.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
 export default class Question extends Component {
 
     render() {
         return (
-            <Text>
-                {this.props.question}
-            </Text>
-        )
+            <View>
+                <Text>
+                    {this.props.question}
+                </Text>
+                <TouchableElement
+                    style={styles.button}
+                    onPress={this.props.onAnswer.bind(this, "agree")}>
+                    <View>
+                        <Text style={styles.buttonText}>Agree!</Text>
+                    </View>
+                </TouchableElement> 
+                <TouchableElement
+                    style={styles.button}
+                    onPress={this.props.onAnswer.bind(this, "disagree")}>
+                    <View>
+                        <Text style={styles.buttonText}>Disagree!</Text>
+                    </View>
+                </TouchableElement> 
+            </View>
+        );
     }
 
 }
