407c659b161957b5fbd8a42d59ff5bd6cc7ec43c
TheZohan <zohar.guy@gmail.com>
2016-06-23 16:53:36 +0300

    FIx path for Android

--- questionnaireapp/index.android.js
+++ questionnaireapp/index.android.js
@@ -6,7 +6,7 @@
 
 import React, { Component } from 'react';
 import {AppRegistry} from 'react-native';
-import QuestionnaireContainer from './src/components/Questionnaire';
+import QuestionnaireContainer from './src/components/QuestionnaireContainer';
 
 class questionnaireapp extends Component {
   render() {
