26c80dd39d12748aba7c81e3877ec1089c9c4823
Eyal Zach <eyal.zach@episte.ma>
2016-06-23 12:02:53 +0300

    first component

---
+++ questionnaireapp/src/components/Questionnaire.jsx
@@ -0,0 +1,27 @@
+
+import React, { Component } from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
+
+export default class Questionnaire extends Component {
+    render() {
+	    <View style={styles.container}>
+        <Text style={styles.welcome}>
+         	Hello, world
+        </Text>
+      </View>
+    }
+}
